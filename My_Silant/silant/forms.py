c66c0d068642d3023be090be352c5ccf350c4fc2
aerastov <a.erastov@gmail.com>
2023-03-12 02:07:14 +0000

    preview 2.2

--- My_Silant/silant/forms.py
+++ My_Silant/silant/forms.py
@@ -7,7 +7,8 @@ now = timezone.now()
 # print("now = ", now.year)
 
 class FactoryNumber(forms.Form):
-    factory_number = forms.CharField(label='Заводской номер:', max_length=20)
+    factory_number = forms.CharField(label='Заводской №', widget=forms.TextInput(attrs={'size': 8}),
+                                     max_length=10)
 
 
 class CreateCarForm(forms.ModelForm):
